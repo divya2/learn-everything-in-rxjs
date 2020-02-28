faf3e290e144e85e220de8c8f0103c095eb2fdd8
divya2 <divu.0209@gmail.com>
2020-02-28 05:48:06 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -14,34 +14,3 @@ const ofArray = of([1,2,3,4], {val: 'Hello'}, function val() {
 ofArray.subscribe(x => console.log(x));
 
 
-
-const textbox = document.getElementById('textbox');
-const input$ = fromEvent(textbox, 'keyup')
-
-const textbox1 = document.getElementById('textbox1');
-const input1$ = fromEvent(textbox, 'keyup')
-
-const textbox2 = document.getElementById('textbox2');
-const input2$ = fromEvent(textbox, 'keyup')
-
-input$.pipe(
-    auditTime(1000),
-    map((val: any) =>  
-      val
-  )).subscribe(console.log);
- /* 
-input1$.pipe(
-    debounceTime(1000),
-    map((val: any) => 
-      val.currentTarget.value
-  )).subscribe(console.log);
-  
-input2$.pipe(
-    throttleTime(1000),
-    map((val: any) => 
-      val.currentTarget.value
-  )).subscribe(console.log);*/
-
-
-
-
